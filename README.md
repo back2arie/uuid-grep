5afd9cac7aa86e7eafa2ed4daa7dc5e18179557d
Azhari Harahap <blackarazi@gmail.com>
2024-08-21 16:16:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# uuid-grep
+
+Simple CLI utility to search UUID from string
+
+## Installation
+
+```bash
+npm install uuid-grep
+```
+
+## Usage
+
+### Oneliner
+
+```bash
+waybackurls target.com | uuid-grep
+```
+
+### From File
+
+```bash
+cat input.txt | uuid-grep
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+function uuidGrep() {
+
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
+
+    const uuidRegex = new RegExp(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/g);
+
+    rl.on('line', (line) => {
+        const uuidMatches = uuidRegex.exec(line);
+
+        if (uuidMatches) {
+            console.log(uuidMatches["input"]);
+        }
+    });
+}
+
+uuidGrep()
+
+module.exports = uuidGrep
